f8ee279ea807b13e9078bf5c307151a9a0e5c20a
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-01-25 15:49:23 +0600

    about us page add Photos Gallery page

--- assets/js/main.js
+++ assets/js/main.js
@@ -335,3 +335,41 @@
     },
   });
 })();
+
+// Gallery swiper js
+
+let swiper = new Swiper('.mySwiper', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 6,
+  breakpoints: {
+    414: {
+      slidesPerView: 2,
+    },
+    576: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 6,
+    },
+  },
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+let swiper2 = new Swiper('.mySwiper2', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
